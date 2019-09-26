9ca1fe00cfb82943c87e057b883308f4f297b55b
yoboom <1369276645@qq.com>
2019-09-26 09:04:45 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="icons">
+     <swiper :options="swiperOption">
+       <swiper-slide v-for="(page,index) of pages" :key="index">
+         <div class="icon" v-for="item of page" :key="item.id">
+           <div class="icon-img">
+             <img :src="item.iconUrl" alt="" class="icon-imgcontent">
+           </div>
+           <p class="icon-desc">{{item.desc}}</p>
+         </div>
+       </swiper-slide>
+     </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        loop: true,
+        autoplay: false
+      },
+      iconList: [{
+        id: '01001',
+        iconUrl: 'static/swiper_icons/swiper-10.png',
+        desc: '机票'
+      }, {
+        id: '01002',
+        iconUrl: 'static/swiper_icons/swiper-11.png',
+        desc: '度假'
+      }, {
+        id: '01003',
+        iconUrl: 'static/swiper_icons/swiper-12.png',
+        desc: '景点'
+      }, {
+        id: '01004',
+        iconUrl: 'static/swiper_icons/swiper-13.png',
+        desc: '火车票'
+      }, {
+        id: '01005',
+        iconUrl: 'static/swiper_icons/swiper-01.png',
+        desc: '周边短途'
+      }, {
+        id: '01006',
+        iconUrl: 'static/swiper_icons/swiper-02.png',
+        desc: '汽车票'
+      }, {
+        id: '01007',
+        iconUrl: 'static/swiper_icons/swiper-03.png',
+        desc: '自驾游'
+      }, {
+        id: '01008',
+        iconUrl: 'static/swiper_icons/swiper-04.png',
+        desc: '民宿'
+      }, {
+        id: '01009',
+        iconUrl: 'static/swiper_icons/swiper-05.png',
+        desc: '自由行'
+      }, {
+        id: '01010',
+        iconUrl: 'static/swiper_icons/swiper-06.png',
+        desc: '特惠酒店'
+      }, {
+        id: '01011',
+        iconUrl: 'static/swiper_icons/swiper-07.png',
+        desc: '海外酒店'
+      }, {
+        id: '01012',
+        iconUrl: 'static/swiper_icons/swiper-08.png',
+        desc: '攻略'
+      }, {
+        id: '01013',
+        iconUrl: 'static/swiper_icons/swiper-09.png',
+        desc: '客栈'
+      }, {
+        id: '01014',
+        iconUrl: 'static/swiper_icons/swiper-14.png',
+        desc: '理财'
+      }, {
+        id: '01015',
+        iconUrl: 'static/swiper_icons/swiper-15.png',
+        desc: '团购'
+      }, {
+        id: '01016',
+        iconUrl: 'static/swiper_icons/swiper-16.png',
+        desc: '邮轮'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height 0
+    padding-bottom 50%
+  .icon
+    position relative
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      left 0
+      top 0
+      right 0
+      bottom 4.2rem
+      text-align center
+      .icon-imgcontent
+        height 100%
+    .icon-desc
+      position absolute
+      height 3.2rem
+      line-height  3.2rem
+      font-size 1.4rem
+      text-align center
+      margin 0 0 1rem 0
+      bottom 0
+      right 0
+      left 0
+      color $darkTextColor
+      ellipsis()
+</style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="wrapper">
-    <swiper :options="swiperOption" >
+  <div class="wrapper swiper-no-swiping">
+    <swiper :options="swiperOption" ref="mySwiper">
         <!-- slides -->
         <swiper-slide v-for="item of swiperList" :key="item.id">
           <img class="swiper-img" :src="item.imgUrl" >
@@ -19,14 +19,35 @@ export default {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true
+        loop: true,
+        autoplay: 3000
+      },
+      computed: {
+        swiper () {
+          return this.$refs.mySwiper.swiper
+        }
+      },
+      mounted () {
+        // current swiper instance
+        // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
+        console.log('this is current swiper instance object', this.swiper)
+        this.swiper.slideTo(3, 1000, false)
       },
       swiperList: [{
         id: '00001',
-        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/d75097876425de8365684f0693b37fd3.jpg'
+        imgUrl: 'static/swiper_img/swiper-01.jpg'
       }, {
         id: '00002',
-        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/d75097876425de8365684f0693b37fd3.jpg'
+        imgUrl: 'static/swiper_img/swiper-02.jpg'
+      }, {
+        id: '00003',
+        imgUrl: 'static/swiper_img/swiper-03.jpg'
+      }, {
+        id: '00004',
+        imgUrl: 'static/swiper_img/swiper-04.jpg'
+      }, {
+        id: '00005',
+        imgUrl: 'static/swiper_img/swiper-05.jpg'
       }]
     }
   }
@@ -47,7 +68,10 @@ export default {
     overflow hidden
     width 100%
     height 0
+    text-align center
     padding-bottom 32.8125%
     .swiper-img
-      width 100%
+      width 95%
+      margin-top 1rem
+      border-radius .7rem
 </style>
