cda0b3b7506b7e032ff462edb6e66e03280f5af3
yoboom <1369276645@qq.com>
2019-09-27 07:06:00 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-content" >
+      <span class="recommend-btn" v-for="(item,index) of btnlist" ref="sp"
+      :key="item.id" @click="handle(index)">
+        {{item.text}}
+      </span>
+      <div class="recommend-more">更多优惠</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      btnlist: [{
+        id: '0101',
+        text: '今日特惠'
+      }, {
+        id: '0102',
+        text: '特价门票'
+      }, {
+        id: '0103',
+        text: '一日游'
+      }]
+    }
+  },
+  methods: {
+    handle (index) {
+      for (let i in this.$refs.sp) {
+        this.$refs.sp[i].style.background = ''
+        this.$refs.sp[i].style.color = ''
+      }
+      this.$refs.sp[index].style.background = '#17c0c8'
+      this.$refs.sp[index].style.color = '#fff'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .recommend
+    height 3.84rem
+    .recommend-content
+      width 95%
+      float right
+      .recommend-btn
+        display inline-block
+        width 9rem
+        height 3.64rem
+        line-height  3.64rem
+        font-size 1.4rem
+        text-align center
+        border .1rem solid #999999
+        margin-right 1rem
+        color #666
+        border-radius .2rem
+      .recommend-more
+        float right
+        height 3.64rem
+        line-height  3.64rem
+        font-size 1.4rem
+        margin-right 2.6rem
+        color #999
+        text-decoration underline
+</style>
